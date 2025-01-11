81fef036647115c72bbc585eec6766a6d0e75af1
Ilya Smut <p2688910@my365.dmu.ac.uk>
2025-01-11 01:55:39 +0000

    added feedback

--- pypasscheck/checker.py
+++ pypasscheck/checker.py
@@ -1,17 +1,77 @@
+from enum import Enum
 import re
 
-def contains_special_characters(s):
-    pattern = r'[ !"#$%&\'()*+,\-./:;<=>?@\[\]^_`{|}]'
-    # Regex to match any non-alphanumeric character
-    return bool(re.search(pattern, s))
-
 class Checker:
+    class Feedback(Enum):
+        STRONG_ENOUGH = 1
+        TOO_SHORT = 2
+        NO_DIGITS = 3
+        NO_ALPHAPHABETIC = 4
+        NO_SPECIAL_CHAR = 5
+
+    @staticmethod
+    def is_long_enough(password: str, expected_len = 8):
+        return len(password) >= expected_len
+    
+
+    @staticmethod
+    def has_digits(password: str):
+        return any(c.isdigit() for c in password)
+    
+
+    @staticmethod
+    def has_alphabetic(password: str):
+        return any(c.isalpha() for c in password)
+
+
+    @staticmethod
+    def contains_special_characters(s):
+        pattern = r'[ !"#$%&\'()*+,\-./:;<=>?@\[\]^_`{|}~]'
+        # Regex to match any non-alphanumeric character
+        return bool(re.search(pattern, s))
+
+
+    @staticmethod
     def is_strong(password: str):
-        if len(password) >= 8 and \
-            any(c.isdigit() for c in password) and \
-            any(c.isalpha() for c in password) and \
-            contains_special_characters(password):
+        if Checker.is_long_enough(password) and \
+            Checker.has_digits(password) and \
+            Checker.has_alphabetic(password) and \
+            Checker.contains_special_characters(password):
             return True
         else:
             return False
 
+
+    @staticmethod
+    def password_feedback(password: str):
+        if not Checker.is_long_enough(password):
+            return Checker.Feedback.TOO_SHORT
+        elif not Checker.has_alphabetic(password):
+            return Checker.Feedback.NO_ALPHAPHABETIC
+        elif not Checker.has_digits(password):
+            return Checker.Feedback.NO_DIGITS
+        elif not Checker.contains_special_characters(password):
+            return Checker.Feedback.NO_SPECIAL_CHAR
+        else:
+            return Checker.Feedback.STRONG_ENOUGH
+
+
+    @staticmethod
+    def password_assessment(password: str):
+        result = []
+        if not Checker.is_long_enough(password):
+            result.append(Checker.Feedback.TOO_SHORT)
+
+        if not Checker.has_alphabetic(password):
+            result.append(Checker.Feedback.NO_ALPHAPHABETIC)
+
+        if not Checker.has_digits(password):
+            result.append(Checker.Feedback.NO_DIGITS)
+
+        if not Checker.contains_special_characters(password):
+            result.append(Checker.Feedback.NO_SPECIAL_CHAR)
+
+        if Checker.is_strong(password):
+            result.append(Checker.Feedback.STRONG_ENOUGH)
+        
+        return result
--- tests.py
+++ tests.py
@@ -1,11 +1,12 @@
 import unittest
 from pypasscheck.checker import Checker
+from random import Random
+import string
 
 special_characters = u'[ !"#$%&\'()*+,-./:;<=>?@[]^_`{|}~]'
 
-print(special_characters)
 
-def generator(special):
+def generate_strong_passwords(special):
     baseline = "abcDE189"
     out = []
     for symbol in special:
@@ -13,20 +14,54 @@ def generator(special):
     return out
 
 
+def generate_weak_password():
+    r = Random()
+    out = []
+    # too short + lowercase only
+    out.append(''.join(r.choices(string.ascii_lowercase, k=7)))
+    # lowercase only
+    out.append(''.join(r.choices(string.ascii_lowercase, k=25)))
+    # no spec characters and no digits
+    out.append(''.join(r.choices(string.ascii_lowercase + string.ascii_uppercase, k=25)))
+    # no spec characters
+    out.append(''.join(r.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k=25)))
+    # no digits
+    out.append(''.join(r.choices(string.ascii_lowercase + string.ascii_uppercase + special_characters, k=25)))
+    return out
+
 
 class TestPasswordComplexity(unittest.TestCase):
+
+
     def test_weak_password(self):
-        self.assertFalse(Checker.is_strong("12345678a"))
+        for password in generate_weak_password():
+            self.assertFalse(Checker.is_strong(password))
+
+
     def test_strong_password(self):
         self.assertTrue(Checker.is_strong("XWZkaOOl@11{23!kk"))
-        for password in generator(special_characters):
-            print(password, end=" ")
-            try:
-                self.assertTrue(Checker.is_strong(password))
-                print("STRONG - PASSED")
-            except AssertionError:
-                print("WEAK - FAILED")
-                raise AssertionError
+        for password in generate_strong_passwords(special_characters):
+            self.assertTrue(Checker.is_strong(password))
+            
+
+    def test_password_feedback(self):
+        too_short = "Aa1@"
+        no_alphabetic = "12345678@!"
+        no_digits = "abcdefghij@"
+        no_spec_char = "ABCdefGHI12345678"
+        good_enough = "IUsa89@!iiiooo.com"
+        self.assertEqual(Checker.password_feedback(too_short), Checker.Feedback.TOO_SHORT)
+        self.assertEqual(Checker.password_feedback(no_alphabetic), Checker.Feedback.NO_ALPHAPHABETIC)
+        self.assertEqual(Checker.password_feedback(no_digits), Checker.Feedback.NO_DIGITS)
+        self.assertEqual(Checker.password_feedback(no_spec_char), Checker.Feedback.NO_SPECIAL_CHAR)
+        self.assertEqual(Checker.password_feedback(good_enough), Checker.Feedback.STRONG_ENOUGH)
+
+    
+    def test_password_assessment(self):
+        psw = "123"
+        result = Checker.password_assessment(psw)
+        self.assertEqual(set(result), {Checker.Feedback.TOO_SHORT, Checker.Feedback.NO_ALPHAPHABETIC, Checker.Feedback.NO_SPECIAL_CHAR})
+        
 
 
 
